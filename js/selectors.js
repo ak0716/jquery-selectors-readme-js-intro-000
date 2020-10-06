b926463a9b79227d405e373e1568ad8ac78fc10a
Ariel K <ar.kogan@gmail.com>
2020-10-06 22:46:00 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
 // declare your functions here...
+function paragraphSelector() {
+  $('p')  
+}
+
+function lastImageSelector() {
+  $('#baby-ninja')
+}
+
+function divSelector() {
+  $('.pics')
+}
+
+function firstListItem() {
+  $('#pic-list:first-child')
+}
